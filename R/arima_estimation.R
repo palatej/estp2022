e3f2e9f67a98a96e852a9b8ea76713faa98e8e28
Palate Jean <jean.palate@nbb.be>
2022-10-08 18:40:58 +0200

    Arima_estimation

--- R/arima_estimation.R
+++ R/arima_estimation.R
@@ -1,6 +1,6 @@
 # JD+
-print(system.time(for (i in 1:50) {  j<-rjd3modelling::sarima.estimate(log(rjd3toolkit::ABS$X0.2.09.10.M), order=c(2,1,1), seasonal = list(order=c(0,1,1), period=12))}))
-print( j$likelihood )
+print(system.time(for (i in 1:1000) {  j<-rjd3modelling::sarima.estimate(log(rjd3toolkit::ABS$X0.2.09.10.M), order=c(2,1,1), seasonal = list(order=c(0,1,1), period=12))}))
 #R-native
-print(system.time(for (i in 1:50) {  r<-arima(log(rjd3toolkit::ABS$X0.2.09.10.M), order=c(2,1,1), seasonal = list(order=c(0,1,1), period=12))}))
+print(system.time(for (i in 1:1000) {  r<-arima(log(rjd3toolkit::ABS$X0.2.09.10.M), order=c(2,1,1), seasonal = list(order=c(0,1,1), period=12))}))
+print( j$likelihood )
 print(r)
