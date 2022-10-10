cbaf64a2d5a2d1592d30ba5a00e9bf07c645cb9e
Palate Jean <jean.palate@nbb.be>
2022-10-10 14:09:32 +0200

    ucarima-estimation

--- R/differencing.R
+++ R/differencing.R
@@ -1,6 +1,4 @@
-suppressPackageStartupMessages(library(rjd3modelling))
-
-s<-log(retail$RetailSalesTotal)
+s<-log(rjd3toolkit::retail$RetailSalesTotal)
 
 plot(s)
 
@@ -10,7 +8,7 @@ pac<-rjd3toolkit::autocorrelations.partial (s, T, n=36)
 all<-cbind(ac, pac)
 barplot(t(all), beside = T, col = c("red", "blue"), names.arg = c(1:36), ylim=c(-1,1))
 
-ds=differences(s,12,F)
+ds=rjd3modelling::differences(s,12,F)
 #plot(ds, type='l')
 
 ac<-rjd3toolkit::autocorrelations(ds, T, n=36)
@@ -19,7 +17,7 @@ pac<-rjd3toolkit::autocorrelations.partial(ds, T, n=36)
 all<-cbind(ac, pac)
 barplot(t(all), beside = T, col = c("red", "blue"), names.arg = c(1:36), ylim=c(-1,1))
 
-ds=differences(ds,1,F)
+ds=rjd3modelling::differences(ds,1,F)
 #plot(ds, type='l')
 
 ac<-rjd3toolkit::autocorrelations(ds, T, n=36)
---
+++ R/ucm_estimation.R
@@ -0,0 +1,33 @@
+airline<-function(period, th, bth){
+  model<-rjd3modelling::sarima.model("airline", period=period, d=1, bd=1, theta = th, btheta = bth)
+  return (model)
+}
+
+model<-airline(12, -.5, -.95)
+ucm<-rjd3modelling::sarima.decompose(model)
+
+y<-rjd3toolkit::retail$ShoeStores
+
+q<-rjd3modelling::ucarima.estimate(ucm, y)
+
+t<-q[,1]
+s<-q[,2]
+i<-q[,3]
+
+sa<-t+i
+
+print(rjd3sa::seasonality.qs(i, 12, 4))
+
+par(mfrow=c(2,1))
+
+M<-max(c(y,t,sa))
+m<-min(c(y,t,sa))
+
+plot(as.numeric(y), type='l', col='gray', ylim=c(m, M))
+lines(sa, col='blue')
+lines(t, col='red')
+grid()
+
+plot(i, type='l', col='magenta')
+
+par(mfrow=c(1,1))
